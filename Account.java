6144156802e0cf385e61d6b6ec08f617a2274c3b
Swizel De Melo <141338620+swiz07@users.noreply.github.com>
2026-09-06 19:28:21 +0100

    Update Account class with login and deposit
    Updated methods to handle double values for deposits and withdrawals. Added PIN functionality for account security.

--- Account.java
+++ Account.java
@@ -5,28 +5,35 @@ public class Account{
   int accountNum;
 
 
-  Account(String name, double balance, int accountNum){
+  Account(String name, double balance, int accountNum, int pin){
     this.name=name;
     this.balance=balance;
     this.accountNum=accountNum;
+    this.pin=pin;
   }
 
 //checks balance method
   void checkBalance(){
-    System.out.println(this.balance);
+    System.out.println("Here is the balance you have: "+ this.balance);
   }
 
 //deposit money method
-  void depositMoney(int deposit){
-    this.balance=this.balance+deposit;
-    System.out.println(this.balance);
+  void depositMoney(double deposit){
+    if(deposit<=0){
+        System.out.println("You cannot deposit");
+    }
+    else{
+        this.balance=this.balance+deposit;
+        System.out.println("Deposit: "+this.balance);
+    }
   }
 
 //withdraw money method
-  void withdrawMoney(int withdraw){
-    if(withdraw>this.balance){
+  void withdrawMoney(double withdraw){
+    if(withdraw>this.balance || withdraw<=0){
         System.out.println("You cannot withdraw");
-    }else{
+    } 
+    else{
       this.balance=this.balance-withdraw;
       System.out.println("withdraw: "+ this.balance);
     }
@@ -36,5 +43,16 @@ public class Account{
   void changePin(int updatePin){
     this.pin=updatePin;
     System.out.println("Your pin has been updated");
+  }
+
+  boolean pinLogin(int input){
+    if(input==this.pin){
+        System.out.println("You are logged in!");
+        return true;
+    }
+    else{
+        System.out.println("Invalid PIN");
+        return false;
+    }
   }
 }
